de2c9468a8157810ee2b71809e1d9316afb6c98d
nonogogono <morinaga_dars_stream@yahoo.co.jp>
2020-08-22 14:10:44 +0900

    heroku url 変更 #17

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'pomodoro3.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'pomodoro3new.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.sendgrid.net",
