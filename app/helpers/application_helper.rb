f2f5eecc81d1b9a7729cb691a3dce23d2f706781
nonogogono <morinaga_dars_stream@yahoo.co.jp>
2020-08-23 17:53:11 +0900

    定数名変更 #19

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   def full_title(page_title: nil)
     if page_title.blank?
       # config/initializers/constants.rb から参照
-      Constants::BASE_TITLE
+      Constants::APP_NAME
     else
-      "#{page_title} - #{Constants::BASE_TITLE}"
+      "#{page_title} - #{Constants::APP_NAME}"
     end
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,26 +3,26 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
   describe "#full_title" do
     context "page_title を指定しない場合" do
-      it "title は BASE_TITLE のみ表示すること" do
-        expect(helper.full_title).to eq(Constants::BASE_TITLE)
+      it "title は APP_NAME のみ表示すること" do
+        expect(helper.full_title).to eq(Constants::APP_NAME)
       end
     end
 
     context "page_title に nil を渡す場合" do
-      it "title は BASE_TITLE のみ表示すること" do
-        expect(helper.full_title(page_title: nil)).to eq(Constants::BASE_TITLE)
+      it "title は APP_NAME のみ表示すること" do
+        expect(helper.full_title(page_title: nil)).to eq(Constants::APP_NAME)
       end
     end
 
     context "page_title に 空文字 を渡す場合" do
-      it "title は BASE_TITLE のみ表示すること" do
-        expect(helper.full_title(page_title: ' ')).to eq(Constants::BASE_TITLE)
+      it "title は APP_NAME のみ表示すること" do
+        expect(helper.full_title(page_title: ' ')).to eq(Constants::APP_NAME)
       end
     end
 
     context "page_title に 有効な文字列 を渡す場合" do
       it "title は page_title を含めて表示すること" do
-        expect(helper.full_title(page_title: 'sample')).to eq("sample - #{Constants::BASE_TITLE}")
+        expect(helper.full_title(page_title: 'sample')).to eq("sample - #{Constants::APP_NAME}")
       end
     end
 
