a6f074b05cfa278a157efaafbe832f8fee1f9fb9
nonogogono <morinaga_dars_stream@yahoo.co.jp>
2020-09-13 18:24:22 +0900

    bullet 導入 #29

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,11 +2,11 @@ class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
       @micropost = current_user.microposts.build
-      @feed_items = current_user.feed.recent.page(params[:page]).per(Constants::FEED_NUM)
+      @feed_items = current_user.feed.includes([:user]).recent.page(params[:page]).per(Constants::FEED_NUM)
       @search_user = User.ransack(params[:q])
       @users = @search_user.result(distinct: true).page(params[:page]).per(Constants::SEARCH_USER_NUM) if params[:q].present?
       @search_micropost = Micropost.ransack(params[:p], search_key: :p)
-      @microposts = @search_micropost.result(distinct: true).recent.page(params[:page]).per(Constants::SEARCH_MICROPOST_NUM) if params[:p].present?
+      @microposts = @search_micropost.result(distinct: true).includes([:user]).recent.page(params[:page]).per(Constants::SEARCH_MICROPOST_NUM) if params[:p].present?
     end
   end
 
