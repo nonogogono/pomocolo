c49e94b7c79454ae5ddbfc177e760ad7b34f8132
nonogogono <morinaga_dars_stream@yahoo.co.jp>
2020-09-13 19:03:07 +0900

    heroku メモリエラー確認のため #29

--- app/assets/javascripts/task_graph.js
+++ app/assets/javascripts/task_graph.js
@@ -1,32 +1,32 @@
 
-$(function() {
-  // 集計の最大日数
-  var term = 31;
+// $(function() {
+//   // 集計の最大日数
+//   var term = 31;
 
-  $('.value').each(function() {
-    var text = $(this).text();
-    $(this).parent().css('width', text*1.5);
-  });
+//   $('.value').each(function() {
+//     var text = $(this).text();
+//     $(this).parent().css('width', text*1.5);
+//   });
 
-  $('.block').tooltip();
+//   $('.block').tooltip();
 
-  for( var i=0; i<=term; i++ ) {
-    var block_count = $(`#chart-${i} .block`).length;
+//   for( var i=0; i<=term; i++ ) {
+//     var block_count = $(`#chart-${i} .block`).length;
 
-    for( var j=0; j<block_count; j++ ) {
-      var block_class = `#chart-${i} .block:nth-of-type(${j+1})`;
-      same_as_legend_color(block_class);
-    }
-  }
+//     for( var j=0; j<block_count; j++ ) {
+//       var block_class = `#chart-${i} .block:nth-of-type(${j+1})`;
+//       same_as_legend_color(block_class);
+//     }
+//   }
 
-  function same_as_legend_color(block_class) {
-    var block_id = $(block_class).attr('id');
+//   function same_as_legend_color(block_class) {
+//     var block_id = $(block_class).attr('id');
 
-    for (var i=0; i<20; i++) {      
-      if( block_id == $(`.legend li:nth-of-type(${i+1})`).text()) {
-        $(block_class).css('background-color', `var(--task-color-${i+1})`);
-        return;
-      }
-    }
-  }
-});
+//     for (var i=0; i<20; i++) {      
+//       if( block_id == $(`.legend li:nth-of-type(${i+1})`).text()) {
+//         $(block_class).css('background-color', `var(--task-color-${i+1})`);
+//         return;
+//       }
+//     }
+//   }
+// });
